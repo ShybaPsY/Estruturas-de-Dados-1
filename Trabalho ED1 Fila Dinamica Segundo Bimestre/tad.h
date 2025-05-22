a1448e1000acf4d16f14556140b527753bfafd75
ShybaPsY <asdgabrielmlopes@gmail.com>
2025-05-22 17:12:48 -0300

    acho q ta certo a exclusão :D

--- Trabalho ED1 Fila Dinamica Segundo Bimestre/tad.h
+++ Trabalho ED1 Fila Dinamica Segundo Bimestre/tad.h
@@ -47,7 +47,7 @@ TpCaixaDev *criarCaixaDev(char nome[50]){
 	strcpy(dev->nome, nome);
 	dev->descTarefas.inicio=NULL;
 	dev->descTarefas.fim=NULL;
-	dev->inicio = dev->fim = NULL;
+	dev->ant = dev->prox = NULL;
 	return dev;
 }
 
@@ -68,8 +68,8 @@ TpCaixaTarefa *criarCaixaTarefa(int prioridade, int tempo, char desc[50], char r
 	return tarefa;
 }
 
-bool devVazio(TpDescDev d){
-	return d.inicio == NULL;
+bool devVazio(TpDescDev dev){
+	return dev.inicio == NULL;
 }
 
 bool tarefaVazia(TpDescDev dev, char nome[50]){
@@ -94,9 +94,13 @@ void inserirDev(TpDescDev &dev, char nome[50]){
 	
 	if(dev.inicio==NULL){
 		dev.inicio = dev.fim = criarCaixaDev(nome);
+		dev.qtde++;
 	}
 	else{
 		dev.fim->prox = criarCaixaDev(nome);
+		dev.fim->prox->ant = dev.fim;
+		dev.fim = dev.fim->prox;
+		dev.qtde++;
 	}
 }
 
@@ -105,11 +109,13 @@ void inserirTarefa(TpDescDev &dev, char nome[50], TpCaixaTarefa *caixa){
 	if(strcmp(dev.inicio->nome, nome) == 0){
 		dev.inicio->descTarefas.fim->prox = caixa;
 		dev.inicio->descTarefas.fim = caixa;
+		dev.inicio->descTarefas.qtde++;
 	}
 	else{
 		if(strcmp(dev.fim->nome, nome) == 0){
 			dev.fim->descTarefas.fim->prox = caixa;
 			dev.fim->descTarefas.fim = caixa;
+			dev.fim->descTarefas.qtde++;
 		}
 		else{
 			TpCaixaDev *atual = dev.inicio;
@@ -119,6 +125,7 @@ void inserirTarefa(TpDescDev &dev, char nome[50], TpCaixaTarefa *caixa){
 			if(strcmp(atual->prox->nome, nome)==0){
 				atual->prox->descTarefas.fim->prox = caixa;
 				atual->prox->descTarefas.fim = caixa;
+				atual->prox->descTarefas.qtde++;
 			}
 		}
 	}
@@ -141,7 +148,50 @@ void excluirDev(TpDescDev &dev, char nome[50]){
 			dev.inicio = NULL;
 		}
 		else{
-			
+			TpCaixaTarefa *caixa = dev.inicio->descTarefas.inicio;
+			TpCaixaDev *atual;
+			while(caixa!=NULL){
+				for(atual = dev.inicio->prox; atual!=NULL; atual=atual->prox){
+					atual->descTarefas.fim->prox=caixa;
+					atual->descTarefas.fim = caixa;
+					caixa = caixa->prox;
+				}
+			}
+			for(atual = dev.inicio->prox; atual!=NULL; atual=atual->prox){
+				atual->descTarefas.fim->prox = NULL;
+			}
+			dev.incio=dev.inicio->prox;
+			delete(dev.inicio->ant);
+			dev.inicio->ant = NULL;
+		}
+	}
+	else{
+		TpCaixaDev *atual = dev.inicio;
+		while(atual->prox != NULL && strcmp(atual->prox->nome, nome)!=0){
+			atual = atual->prox;
+		}
+		if(strcmp(atual->prox->nome, nome)==0){
+			TpCaixaDev *excluir = atual->prox;
+			TpCaixaTarefa *caixa = excluir->descTarefas.inicio;
+			while(caixa!=NULL){
+				for(atual = dev.inicio;atual!=NULL;atual=atual->prox){
+					if(atual == excluir){
+						atual=atual->prox;
+					}
+					atual->descTarefas.fim->prox= caixa;
+					atual->descTarefas.fim = caixa;
+					caixa = caixa->prox;
+				}
+			}
+			for(atual = dev.inicio->prox; atual!=NULL; atual=atual->prox){
+				if(atual == excluir){
+					atual=atual->prox;
+				}
+				atual->descTarefas.fim->prox = NULL;
+			}
+			excluir->ant->prox=excluir->prox;
+			excluir->prox->ant=excluir->ant;
+			delete(excluir);
 		}
 	}
 }
